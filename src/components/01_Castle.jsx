ad09b72468ce4e905d86a785d2a3ac5f206642ab
inatbalthazar <inatbalthazar@gmail.com>
2026-08-14 10:33:26 +0800

    JS(React): 01_castle.jsx-Done

---
+++ src/components/01_Castle.jsx
@@ -0,0 +1,9 @@
+import SecretRoom from "./09-SecretRoom";
+
+export default function Castle() {
+    return (
+        <div className="flex flex-col justify-center items-center pt-10 bg-purple-400 w-[90%]">
+            <h1>Castle</h1>
+        </div>
+    );
+}
---
+++ src/components/08_Nook.jsx
@@ -0,0 +1,9 @@
+import SecretRoom from "./09-SecretRoom";
+
+export default function Nook() {
+    return (
+        <div className="flex flex-col justify-center items-center pt-10 bg-purple-400 w-[90%]">
+            <h1>Nook</h1>
+        </div>
+    );
+}
